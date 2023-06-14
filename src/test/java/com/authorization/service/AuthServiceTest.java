b4955a59e90381efcf5ecd6f4bf23439ba91c51f
rkgol <iamrkgolu@gmail.com>
2023-06-14 11:05:48 +0530

    code refactor

--- src/test/java/com/authorization/service/AuthServiceTest.java
+++ src/test/java/com/authorization/service/AuthServiceTest.java
@@ -1,50 +1,64 @@
-//package com.authorization.service;
-//
-//import com.authorization.model.User;
-//import com.authorization.repository.UserRepository;
-//import jakarta.servlet.ServletException;
-//import net.bytebuddy.implementation.bytecode.Throw;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.mockito.stubbing.OngoingStubbing;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//@AutoConfigureMockMvc
-//@SpringBootTest
-//public class AuthServiceTest {
-//    @Mock
-//    private UserService userService;
-//
-//    @InjectMocks
-//    private AuthService authService;
-//
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @BeforeEach
-//    public void init()
-//    {
-//        MockitoAnnotations.openMocks(this);
-//        mockMvc = MockMvcBuilders.standaloneSetup(authService).build();
-//    }
-//    List<User> userList = new ArrayList<User>();
+package com.authorization.service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.authorization.model.User;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class AuthServiceTest {
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private AuthService authService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init()
+    {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(authService).build();
+    }
+    List<User> userList = new ArrayList<User>();
+    @Test
+    public void generateTokenSuccess() throws Exception
+    {
+        User user = new User();
+        user.setLoginId("1234");
+        user.setFirstName("Rahul");
+        user.setLastName("Kumar");
+        user.setEmail("iamrkgolu@gmail.com");
+        user.setPassword("Rahul@123");
+        user.setConfirmPassword("Rahul@123");
+        user.setContactNumber("8210526146");
+
+        userList.add(user);
+        when(userService.loginUser(user.getLoginId(),user.getPassword())).thenReturn(true);
+
+        String token = authService.generateToken(user.getLoginId(),user.getPassword());
+        assertTrue(token!=null);
+
+    }
 //    @Test
-//    public void generateTokenSuccess() throws Exception
+//    public void generateTokenFailure() throws ServletException, Exception
 //    {
 //        User user = new User();
 //        user.setLoginId("1234");
@@ -56,26 +70,7 @@
 //        user.setContactNumber("8210526146");
 //
 //        userList.add(user);
-//        when(userService.loginUser(user.getLoginId(),user.getPassword())).thenReturn(true);
-//
-//        String token = authService.generateToken(user.getLoginId(),user.getPassword());
-//        assertTrue(token!=null);
-//
+//        when(userService.loginUser(user.getLoginId(),user.getPassword())).thenReturn(false).then(authService.generateToken("","")).thenThrow(ServletException.class);
+//        assertEquals(authService.generateToken("",""),new ServletException().getMessage());
 //    }
-////    @Test
-////    public void generateTokenFailure() throws ServletException, Exception
-////    {
-////        User user = new User();
-////        user.setLoginId("1234");
-////        user.setFirstName("Rahul");
-////        user.setLastName("Kumar");
-////        user.setEmail("iamrkgolu@gmail.com");
-////        user.setPassword("Rahul@123");
-////        user.setConfirmPassword("Rahul@123");
-////        user.setContactNumber("8210526146");
-////
-////        userList.add(user);
-////        when(userService.loginUser(user.getLoginId(),user.getPassword())).thenReturn(false).then(authService.generateToken("","")).thenThrow(ServletException.class);
-////        assertEquals(authService.generateToken("",""),new ServletException().getMessage());
-////    }
-//}
+}
--- src/test/java/com/authorization/service/UserServiceImplementationTest.java
+++ src/test/java/com/authorization/service/UserServiceImplementationTest.java
@@ -1,64 +1,64 @@
-//package com.authorization.service;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.Mockito.when;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//
-//import com.authorization.model.User;
-//import com.authorization.repository.UserRepository;
-//
-//@AutoConfigureMockMvc
-//@SpringBootTest
-//public class UserServiceImplementationTest {
-//	
-//	@Mock
-//	private UserRepository userRepository;
-//	
-//	@InjectMocks
-//	private UserServiceImplementation userService;
-//	
-//	@Autowired
-//	private MockMvc mockMvc;
-//	
-//	@BeforeEach
-//	public void init()
-//	{
-//		MockitoAnnotations.openMocks(this);
-//		mockMvc = MockMvcBuilders.standaloneSetup(userService).build();
-//	}
-//	List<User> userList = new ArrayList<User>();
-//	
-//	@Test
-//	public void getAllUsersSuccess() throws Exception
-//	{
-//		User user = new User();
-//		user.setLoginId("1234");
-//		user.setFirstName("Rahul");
-//		user.setLastName("Kumar");
-//		user.setEmail("iamrkgolu@gmail.com");
-//		user.setPassword("Rahul@123");
-//		user.setConfirmPassword("Rahul@123");
-//		user.setContactNumber("8210526146");
-//		
-//		userList.add(user);
-//		when(userRepository.findAll()).thenReturn(userList);
-//		
-//		List<User> uList = userService.getAllUsers();
-//		assertEquals(userList, uList);
-//		
-//	}
-//
-//}
+package com.authorization.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.authorization.model.User;
+import com.authorization.repository.UserRepository;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UserServiceImplementationTest {
+	
+	@Mock
+	private UserRepository userRepository;
+	
+	@InjectMocks
+	private UserServiceImplementation userService;
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@BeforeEach
+	public void init()
+	{
+		MockitoAnnotations.openMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(userService).build();
+	}
+	List<User> userList = new ArrayList<User>();
+	
+	@Test
+	public void getAllUsersSuccess() throws Exception
+	{
+		User user = new User();
+		user.setLoginId("1234");
+		user.setFirstName("Rahul");
+		user.setLastName("Kumar");
+		user.setEmail("iamrkgolu@gmail.com");
+		user.setPassword("Rahul@123");
+		user.setConfirmPassword("Rahul@123");
+		user.setContactNumber("8210526146");
+		
+		userList.add(user);
+		when(userRepository.findAll()).thenReturn(userList);
+		
+		List<User> uList = userService.getAllUsers();
+		assertEquals(userList, uList);
+		
+	}
+
+}
